b29f85f7d3ab72db6fb7370a5921cd048f919538
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-25 19:39:49 -0300

    finishing frist day

--- with-styled-components-app/src/components/head/index.js
+++ with-styled-components-app/src/components/head/index.js
@@ -14,7 +14,7 @@ function IndexPage() {
         <meta property="og:url" content="https://myquiz.vercel.app/"/>
         <meta property="og:title" content="Quiz CSS da Alura"/>
         <meta property="og:description" content=""/>
-        <meta property="og:image" content="https://i2.wp.com/css-tricks.com/wp-content/uploads/2017/06/css-is-awesome-scaled.jpg?resize=1536%2C1208&ssl=1"/>
+        <meta property="og:image" content="https://media.istockphoto.com/photos/glass-with-whiskey-on-wooden-table-and-black-background-copy-space-picture-id1213740620?k=6&m=1213740620&s=170667a&w=0&h=GhRyfLC6hjD1P-rZ2hqi19rmiSg1kAIiy26sNuBHX_0="/>
         </Head>
 
       </div>
