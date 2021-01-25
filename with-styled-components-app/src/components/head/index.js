81b44bfc680156f641a64db542c88bce3baa10f1
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-25 12:42:31 -0300

    trying to use OG

---
+++ with-styled-components-app/src/components/head/index.js
@@ -0,0 +1,21 @@
+function IndexPage() {
+    return (
+      <div>
+        <Head>
+          <title>Quiz CSS da Alura</title>
+          <meta property="og:title" content="Quiz CSS da Alura" key="title" />
+          <meta name="description" content="construir um quiz para brincar com os amigos feito em Next.js  " />
+        </Head>
+        <Head>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://myquiz.vercel.app/"/>
+        <meta property="og:title" content="Quiz CSS da Alura"/>
+        <meta property="og:description" content=""/>
+        <meta property="og:image" content="https://i2.wp.com/css-tricks.com/wp-content/uploads/2017/06/css-is-awesome-scaled.jpg?resize=1536%2C1208&ssl=1"/>
+        </Head>
+        <p>Hello world!</p>
+      </div>
+    )
+  }
+  
+  export default IndexPage
