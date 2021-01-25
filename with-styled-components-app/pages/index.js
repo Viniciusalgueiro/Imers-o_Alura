35bf32d9f9eb3761fb35a64b43069d7862c6973c
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-25 12:35:57 -0300

    adding OG-title

--- with-styled-components-app/pages/index.js
+++ with-styled-components-app/pages/index.js
@@ -5,7 +5,29 @@ import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import Head from 'next/head'
 
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Quiz CSS da Alura</title>
+        <meta property="og:title" content="Quiz CSS da Alura" key="title" />
+        <meta name="description" content="construir um quiz para brincar com os amigos feito em Next.js  " />
+      </Head>
+      <Head>
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://myquiz.vercel.app/"/>
+      <meta property="og:title" content="Quiz CSS da Alura"/>
+      <meta property="og:description" content=""/>
+      <meta property="og:image" content="https://i2.wp.com/css-tricks.com/wp-content/uploads/2017/06/css-is-awesome-scaled.jpg?resize=1536%2C1208&ssl=1"/>
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default IndexPage
 
 export const QuizContainer = styled.div`
   width: 100%;
