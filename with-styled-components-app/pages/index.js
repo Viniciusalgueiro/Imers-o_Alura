fe0fd5aaaaaa2dcdbcaab533ceab06a74db14388
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-01-28 22:48:27 -0300

    fixing

--- with-styled-components-app/pages/index.js
+++ with-styled-components-app/pages/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-import IndexPage from '../src/components/head';
 import Input from '../src/components/input';
 import Button from '../src/components/Button';
-import QuizContainer from '../src/components/Quizcontainer';
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <IndexPage />
+      <Head>
+        <title>{db.title}</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
@@ -25,29 +39,29 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
+            <p>{db.description}</p>
             <form onSubmit={function (infosDoEvento) {
               infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
-              console.log('sumbmitted ');
+              console.log('Fazendo uma submissão por meio do react');
             }}
             >
               <Input
                 name="nomeDoUsuario"
-                onChange={(infosDoEvento) => {
-                  setName(infosDoEvento.target.value);
-                }}
-                placeholder="insira seu nome"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
                 value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
-                {`jogar ${name}`}
+                {`Jogar ${name}`}
               </Button>
             </form>
           </Widget.Content>
         </Widget>
+
         <Widget>
           <Widget.Content>
-            <h1>Outros Quizes</h1>
+            <h1>Quizes da Galera</h1>
 
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
