bb3b0de52c314e92e98ed5834c879850036e14f3
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-02-06 14:05:14 -0300

    aula 5

--- with-styled-components-app/pages/Quiz/index.js
+++ with-styled-components-app/pages/Quiz/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizContainer from '../src/components/Quizcontainer';
-import AlternativesForm from '../src/components/AlternativeForm';
-import Button from '../src/components/Button';
+import db from '../../db.json';
+import Widget from '../../src/components/Widget';
+import QuizLogo from '../../src/components/QuizLogo';
+import QuizBackground from '../../src/components/QuizBackground';
+import QuizContainer from '../../src/components/Quizcontainer';
+import AlternativesForm from '../../src/components/AlternativeForm';
+import Button from '../../src/components/Button';
 
 const Spin = keyframes`
 to{ transform: rotate(360deg);}
